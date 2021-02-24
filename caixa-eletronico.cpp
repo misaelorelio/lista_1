4450aa03941b9715fba29e53fedb2497589126d9
misaelorelio <misaelorelio@hotmail.com>
2021-02-23 21:19:44 -0300

    estrutura de decisao

--- caixa-eletronico.cpp
+++ caixa-eletronico.cpp
@@ -4,13 +4,12 @@ main (){
 	printf("Informe o valor de saque minimo R$ 10 maximo R$600: ");
 	scanf("%i", &valor);
 	
-	cem = valor / 100;
-	cinquenta = (valor % 100) / 50;
-	dez = (cinquenta % 100) /
+	cem = ((valor - (valor%100))/100);
+	cinquenta = ((valor%100)/50);
+	dez = (((valor%50)/10));
+	cinco = (((valor%10)/4));
+	um=  ((valor%5));
 	
-	 10;
-	/*cinco = (dez % 100) % 5;
-	um = (cinco % 100) % 1;*/
 	printf("R$100:%i\nR$50:%i\nR$10:%i\nR$5:%i\nR$1:%i" , cem, cinquenta, dez, cinco, um);
 	
 }
--- numero-impar-par.cpp
+++ numero-impar-par.cpp
@@ -1,18 +1,15 @@
 #include<stdio.h>
 main (){
-	int numero, numero1;
+	int resultado, numero1;
 	printf("Informe um numero: ");
 	scanf("%i", &numero1);
-	numero = numero1 / 2;
+	resultado = numero1 % 2;
 	
 	
-	if(numero % 100 == 0 ){
+	if(resultado == 0 ){
 		printf("Par");
 	}
-/*	else if(numero%50 == 0) {
-		printf("nao sei");
-	}*/
-	else{
-		printf("impar");
+	else {
+		printf("Impar");
 	}
 }
