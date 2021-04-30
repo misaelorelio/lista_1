041e3ee2430cf521818b3f1cf0ff58df8b537b0e
misaelorelio <misaelorelio@hotmail.com>
2021-04-30 16:16:37 -0300

    assassino perguntas concluido

--- assassino.cpp
+++ assassino.cpp
@@ -1,6 +1,7 @@
 #include<stdio.h >
 main () {
 	int resposta1, resposta2, resposta3, resposta4, resposta5, soma;
+	
 	printf("Telefonou para a vítima? (sim 1) ou (nao 0)");
 	scanf ("%i", &resposta1);
 	
@@ -21,51 +22,15 @@ main () {
 	
 	
 	if(soma == 2){	
-		printf("Pessoa suspeita");
-  		if(soma == 3 && soma == 4){	
-			printf("Pessoa Cumplice");
-			if(soma == 5){	
-				printf("Assassino");
-		}
+		printf("Pessoa suspeita\n");
 	}
-}
-	else{
-		printf("Pessoa nao suspeita");
+	else if(soma == 3 || soma == 4){
+		printf("Cumplice do assassinato\n");
 	}
-	/*if(soma == 2){	
-		printf("Pessoa suspeita");
-  	}
-  	else{
-		printf("Pessoa nao suspeita");
-	}
-	if(soma > 2 && soma < 4){	
-		printf("Pessoa Cumplice");
+	else if(soma == 5){
+		printf("Assassino\n");
 	}
 	else{
-		printf("Pessoa nao suspeita");
+		printf("Inocente");
 	}
-	if(soma == 5){	
-		printf("Assassino");
-	}
-	else{
-		printf("Pessoa nao suspeita");
-	}*/
-
-	/*if(resposta1 == resposta2 || resposta1 == resposta3 || resposta1 == resposta4 || 
-	resposta1 == resposta5){
-		printf("Pessoa suspeita");
-	}
-	else{
-		printf("Pessoa nao suspeita");
-	}
-	
-	if(resposta2 != resposta3 || resposta2 != resposta4 || resposta2 != resposta5){
-		printf("Pessoa nao suspeita");
-	}
-	else{
-		printf("Cumplice");
-	}*/
-	
-	
-
 }
