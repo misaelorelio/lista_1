e3a6ba4fb562d14993e430170969bcdcd362625d
misaelorelio <misaelorelio@hotmail.com>
2021-05-06 21:16:37 -0300

    Combústivel concluido

--- combustivelDesconto.cpp
+++ combustivelDesconto.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h >
 main () {
-	float litro, precoA, precoG, total, combustivel, desconto1, desconto2;
+	float litro, precoA, precoG, total, combustivel, desconto1, desconto2, volume, volume2;
 	printf("Quantos litros de combustivel? ");
 	scanf ("%f", &litro);
 	
@@ -9,30 +9,30 @@ main () {
 	
 	precoA = 1.90;
 	precoG = 2.50;
-	desconto1 = litro * 0.03;
-	desconto2 = litro * 0.05;
+
 	
 	if(litro <= 20 && combustivel == 1){
-		total = (precoA * litro) - desconto1;
+		volume = (precoA * litro);
+		desconto1 = volume * 0.03;
+		total = volume - desconto1;
+		printf("Total de gastos R$ %.2f ", total);
+	}
+	else if(litro > 20 && combustivel == 1) {
+		volume2 = (precoA * litro);
+		desconto2= volume2 * 0.05;
+		total = volume2 - desconto2;
+		printf("Total de gastos R$ %.2f ", total);
+	}
+	else if(litro <= 20 && combustivel == 2){
+		volume = (precoG * litro);
+		desconto1 = volume * 0.04;
+		total = volume - desconto1;
 		printf("Total de gastos R$ %.2f ", total);
 	}
 	else if(litro > 20 && combustivel == 2) {
-		total = (precoA * litro) - desconto2;
+		volume2 = (precoG * litro);
+		desconto2= volume2 * 0.06;
+		total = volume2 - desconto2;
 		printf("Total de gastos R$ %.2f ", total);
 	}
-	/*desconto1 = (quantidade * 0.03);
-	desconto2 = (quantidade * 0.05);
-	valor = (quantidade * 1.90) ;
-	total1 = valor - desconto1;
-	total2 = valor - desconto2;
-	alcool = quantidade;
-	
-	if(alcool <= 20){
-		printf("Valor de desconto: %.2f\n ",desconto1);
-		printf("Valor de desconto:%.2f\n",total1);
-	}
-	else if(alcool > 20){
-		printf("Valor de desconto: %.2f ",desconto2);
-		printf("Total R$:%.2f ",total2);
-	}*/
 }
