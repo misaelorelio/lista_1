6d46708c4b426ea8ab65cff317e0984d839716b4
misaelorelio <misaelorelio@hotmail.com>
2021-05-06 21:36:37 -0300

    Frutaria finalizado

--- frutaria.cpp
+++ frutaria.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h >
 main () {
-	float frutas, kg, morango, maca;
+	float frutas, kg, morango, maca, total;
 	printf("Quantos quilos de fruta? ");
 	scanf ("%f", &kg);
 	
@@ -10,7 +10,22 @@ main () {
 	
 
 	if(frutas == 1 && kg <= 5){
-		morango = kg * 2.5;
-		printf("Preco da sua compra R$ %.2f\n", morango);
+		total = kg * 2.5;
+		printf("Preco da sua compra R$ %.2f\n", total);
+	}
+	else if(frutas == 2 && kg <= 5){
+		total = kg * 1.8;
+		printf("Preco da sua compra R$ %.2f\n", total);
+	}
+	else if(frutas == 1 && kg > 5 && kg < 8){
+		total = kg * 2.2;
+		printf("Preco da sua compra R$ %.2f\n", total);
+	}
+	else if(frutas == 2 && kg > 5 && kg < 8){
+		total = kg * 1.5;
+		printf("Preco da sua compra R$ %.2f\n", total);
+	}
+	else if(kg > 8 || total > 25){
+		printf("Voce recebera 10 porcento de desconto!");
 	}
 }
