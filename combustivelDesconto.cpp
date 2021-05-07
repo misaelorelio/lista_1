6a9f860182e524c0a391d9783e5012a219d5b970
misaelorelio <misaelorelio@hotmail.com>
2021-05-06 21:01:28 -0300

    Combústivel testando mais

--- combustivelDesconto.cpp
+++ combustivelDesconto.cpp
@@ -1,16 +1,26 @@
 #include<stdio.h >
 main () {
-	float tipo, quantidade1;
-	float alcool, valor, quantidade, desconto1, desconto2, bomba1, total1, total2;
-	
+	float litro, precoA, precoG, total, combustivel, desconto1, desconto2;
 	printf("Quantos litros de combustivel? ");
-	scanf ("%f", &quantidade);
+	scanf ("%f", &litro);
 	
-	printf("Alccol ou gasolina? ");
-	scanf ("%f", &alcool);
+	printf("Alccol - 1 ou Gasolina - 2 ");
+	scanf ("%f", &combustivel);
 	
+	precoA = 1.90;
+	precoG = 2.50;
+	desconto1 = litro * 0.03;
+	desconto2 = litro * 0.05;
 	
-	desconto1 = (quantidade * 0.03);
+	if(litro <= 20 && combustivel == 1){
+		total = (precoA * litro) - desconto1;
+		printf("Total de gastos R$ %.2f ", total);
+	}
+	else if(litro > 20 && combustivel == 2) {
+		total = (precoA * litro) - desconto2;
+		printf("Total de gastos R$ %.2f ", total);
+	}
+	/*desconto1 = (quantidade * 0.03);
 	desconto2 = (quantidade * 0.05);
 	valor = (quantidade * 1.90) ;
 	total1 = valor - desconto1;
@@ -24,5 +34,5 @@ main () {
 	else if(alcool > 20){
 		printf("Valor de desconto: %.2f ",desconto2);
 		printf("Total R$:%.2f ",total2);
-	}
+	}*/
 }
