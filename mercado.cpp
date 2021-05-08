13197d361108f6ce1747446eaf90b5fef276498f
misaelorelio <misaelorelio@hotmail.com>
2021-05-08 12:30:48 -0300

    Mercado if desconto e if sem desconto

--- mercado.cpp
+++ mercado.cpp
@@ -8,7 +8,7 @@ main () {
 	printf("Quantos Kg? ");
 	scanf ("%f", &kgCarne);
 	
-	printf("Pagamento? cartao Tabajara 1 ou Formas de pagamento difdrente 0 ");
+	printf("Pagamento: cartao Tabajara (1) ou Formas de pagamento difdrente (0): ");
 	scanf ("%f", &cartao);
 	
 
@@ -20,6 +20,17 @@ main () {
 	}
 	else if(tipo == 1 && kgCarne <= 5 && cartao == 0) {
 		precoTotal = kgCarne * 4.9;
+		printf("Preco da sua compra R$ %.2f\n", precoTotal);
+	}
+	
+	if(tipo == 2 && kgCarne <= 5 && cartao == 1){
+		precoTotal = kgCarne * 5.9;
+		desconto = precoTotal * 0.05;
+		pagar = precoTotal - desconto;
+		printf("Preco da sua compra R$ %.2f\n", pagar);
+	}
+	else if(tipo == 2 && kgCarne <= 5 && cartao == 0) {
+		precoTotal = kgCarne * 5.9;
 		printf("Preco da sua compra R$ %.2f\n", precoTotal);
 	}
 }
