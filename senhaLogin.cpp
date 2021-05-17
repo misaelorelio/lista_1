6d9d93c67b695ea95bd2baa2044274fe8a101175
misaelorelio <misaelorelio@hotmail.com>
2021-05-17 19:42:24 -0300

    Senha e login

--- senhaLogin.cpp
+++ senhaLogin.cpp
@@ -2,19 +2,14 @@
 main () {
 	
 	int nome, senha;
+	char login [10];
 	printf("Informe seu nome: ");
 	scanf ("%i", &nome);
 	
 	printf("Informe sua senha: ");
 	scanf ("%i", &senha);
 	
-	if(nota > 10 || nota < 0){
-		printf("Informe um valor valido");
-		printf("Informe uma nota entre 0 e 10:  ");
-		scanf ("%i", &nota);
-	}
-	else{
-		printf("valor valido");
-	}
-	
+	printf("Informe %i", nome);
+	printf("Informe %i", senha);
+	printf("Informe %c", login);
 }
