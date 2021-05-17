bad9d163bc251d82633f2c76f24b6ce70a464190
misaelorelio <misaelorelio@hotmail.com>
2021-05-17 19:27:31 -0300

    Algoritmo de nota

--- notaContinuarPedindo.cpp
+++ notaContinuarPedindo.cpp
@@ -5,5 +5,11 @@ main () {
 	printf("Informe uma nota entre 0 e 10:  ");
 	scanf ("%i", &nota);
 	
+	if(nota > 10 || nota < 0){
+		printf("Informe um valor valido");
+	}
+	else{
+		printf("valor valido");
+	}
 	
 }
