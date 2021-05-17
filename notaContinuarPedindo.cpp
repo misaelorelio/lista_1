881fbce9a8f39279a77c60d384714816dbccba5e
misaelorelio <misaelorelio@hotmail.com>
2021-05-17 19:23:54 -0300

    Algoritmo de nota

---
+++ notaContinuarPedindo.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h >
+main () {
+	float  kgCarne, desconto, pagar, precoTotal, cartao;
+	int tipo;
+	printf("Qual carne voce vai querer? 1 -File duplo, 2 -Alcatra, 3 -Picanha  ");
+	scanf ("%i", &tipo);
+	
+	
+}
