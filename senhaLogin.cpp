90fd6d5ee4836113dc963e4e536ca04a2cd008eb
misaelorelio <misaelorelio@hotmail.com>
2021-05-17 19:53:32 -0300

    Senha e login

--- senhaLogin.cpp
+++ senhaLogin.cpp
@@ -4,12 +4,18 @@ main () {
 	
 	char login [10];
 	char senha [10];
+	
 	printf("Informe seu login: ");
 	scanf ("%s", &login);
 	
 	printf("Informe sua senha: ");
 	scanf ("%s", &senha);
 	
-	printf("Informe %s\n", login);
-	printf("Informe %s", senha);
+	
+	if(("%s",login ) == ("%s",senha)){
+		printf("Favor colocar dados diferentes!%s%s", login, senha);
+	}
+	else{
+		printf("Dados cadastrados com sucesso %s", login);
+	}
 }
